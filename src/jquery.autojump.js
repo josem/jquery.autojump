d4ca32022d51c67fc3a40c19c8c5032b92f1029a
José M. Gilgado <jm.gilgado@gmail.com>
2014-11-10 16:48:29 +0100

    Small refactoring to adjust to style

--- src/jquery.autojump.js
+++ src/jquery.autojump.js
@@ -1,38 +1,29 @@
 // the semi-colon before function invocation is a safety net against concatenated
 // scripts and/or other plugins which may not be closed properly.
 ;(function ( $, window, document, undefined ) {
-		var pluginName = "autoJump",
-				defaults = {
-				propertyName: "value"
-		};
+		var pluginName = "autoJump";
 
-		function Plugin ( element, options ) {
+		function Plugin (element) {
 				this.element = element;
-				this.settings = $.extend( {}, defaults, options );
-				this._defaults = defaults;
 				this._name = pluginName;
 				this.init();
 		}
 
 		$.extend(Plugin.prototype, {
 				init: function () {
-          // You already have access to the DOM element and
-          // the options via the instance, e.g. this.element
-          // and this.settings
-          // you can add more functions like the one below and
-          // call them like so: this.yourOtherFunction(this.element, this.settings).
-          $(this.element).on('keypress paste',this.controlKeyStrokes);
+          $(this.element).on("keypress paste",this.controlKeyStrokes);
 				},
+
         controlKeyStrokes: function (event) {
           var currentLength = $(this).val().length;
-          var maxLength = $(this).data('autojump');
+          var maxLength = $(this).data("autojump");
 
-          if (event.type == 'keypress') {
+          if (event.type === "keypress") {
             maxLength -= 1;
           }
 
           if (currentLength >= maxLength) {
-            $(this).nextAll('input,textarea').focus();
+            $(this).nextAll("input,textarea").focus();
           }
         }
 		});
