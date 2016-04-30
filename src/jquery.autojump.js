ca41ab694f00beda0884cca6f5799439832ac271
José M. Gilgado <jm.gilgado@gmail.com>
2016-05-01 00:51:50 +0200

    Updated with fix for #2

--- src/jquery.autojump.js
+++ src/jquery.autojump.js
@@ -10,17 +10,30 @@
     }
 
     $.extend(Plugin.prototype, {
+        fields: [],
+        index: 0,
+
         init: function () {
-            $(this.element).on("keypress paste", this.controlKeyStrokes);
+            this.fields = $(this.element).find("input,select,textarea");
+            $(this.fields).on("keypress paste", {object: this}, this.controlKeyStrokes);
         },
 
-        controlKeyStrokes: function () {
+        controlKeyStrokes: function (e) {
+            var object = e.data.object;
             var currentLength = $(this).val().length;
             var maxLength = $(this).data("autojump");
 
-            if (currentLength === maxLength - 1) {
+            if (currentLength >= maxLength) {
+                e.preventDefault();
+            }
+
+            if (currentLength >= maxLength - 1) {
                 $(this).one("keyup", function() {
-                    $(this).nextAll("input,textarea,select").first().focus();
+                    $(object.fields[++object.index]).focus();
+
+                    if ($(object.fields[object.index]).is("input[type=submit]")) {
+                        object.index = 0;
+                    }
                 });
             }
         }
